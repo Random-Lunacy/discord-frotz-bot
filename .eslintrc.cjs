acad10dc1c0e8a00f991d513e992e8cf73555052
NepoKama <mitch@eatough.net>
2023-09-13 04:42:39 +0000

    Initial framework

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ]
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
