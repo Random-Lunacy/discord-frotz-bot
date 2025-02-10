ed94895e69587a2d8fbea7085b30ca61b268c0c8
NepoKama <mitch@eatough.net>
2025-02-09 18:41:13 -0800

    lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,24 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
     },
-    "plugins": ["sonarjs"],
-    "extends": ["plugin:sonarjs/recommended"],
-    "overrides": [
+    plugins: ['sonarjs'],
+    extends: ['plugin:sonarjs/recommended'],
+    overrides: [
         {
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ]
-        }
+            files: ['.eslintrc.{js,cjs}'],
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    },
 };
